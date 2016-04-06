3d93ccfdf443dbb2d0e9835101c6ae0a50630009
Yondy Kang <yokangy@gmail.com>
2016-04-05 23:13:15 -0700

    Fixed a bug to retrieve the correct action payload.

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -5,7 +5,7 @@ import {
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_USERS:
-      return [...state,...actions.payload];
+      return [...state,...action.payload];
   }
 
   return state;
